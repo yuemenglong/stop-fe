5395a5093fe800994dd0cdc14b27191addf7484d
yuemenglong <yuemenglong@126.com>
2017-12-13 17:52:51 +0800

    add: 将question统一到curd框架下

--- src/Admin/Course/QuestionInfo/index.tsx
+++ src/Admin/Course/QuestionInfo/index.tsx
@@ -11,47 +11,54 @@ import {Simulate} from "react-dom/test-utils";
 import submit = Simulate.submit;
 import {questionTypeMap} from "../../../def/data";
 import {Link} from "react-router-dom";
+import {EH, TEH} from "../../../common/render-component";
 
 class State {
     question: Question = new Question();
 }
 
+interface Props {
+    question: Question;
+    onChange: TEH<Question>;
+    onSubmit: EH;
+    onCancel: EH;
+}
 
-export class QuestionInfo extends RenderPairComponent<RouteComponentProps<any>, State> {
+export class QuestionInfo extends RenderPairComponent<Props, State> {
     getRenderRootMode(): { root: any; mode: string } {
-        return {root: this.state, mode: "state"};
+        return {root: this.props.question, mode: "props"};
     }
 
-    getCid() {
-        return this.props.match.params.id;
-    }
-
-    getQid() {
-        return this.props.match.params.qid;
-    }
+    // getCid() {
+    //     return this.props.match.params.id;
+    // }
+    //
+    // getQid() {
+    //     return this.props.match.params.qid;
+    // }
 
     constructor() {
         super();
         this.state = new State();
     }
 
-    componentDidMount() {
-        if (this.getQid() != "init") {
-            ajax({
-                url: `/course/${this.getCid()}/question/${this.getQid()}`,
-                type: "GET",
-                success: (q) => {
-                    this.setState({question: q})
-                }
-            })
-        } else {
-            let question = _.defaults({ty: "sc"}, this.state.question);
-            this.setState({question: question})
-        }
-    }
+    // componentDidMount() {
+    //     if (this.getQid() != "init") {
+    //         ajax({
+    //             url: `/course/${this.getCid()}/question/${this.getQid()}`,
+    //             type: "GET",
+    //             success: (q) => {
+    //                 this.setState({question: q})
+    //             }
+    //         })
+    //     } else {
+    //         let question = _.defaults({ty: "sc"}, this.props.question);
+    //         this.setState({question: question})
+    //     }
+    // }
 
     renderContent() {
-        if (this.state.question.ty == "sc") {
+        if (this.props.question.ty == "sc") {
             let answerMap = {
                 "": "请选择",
                 "a": "选项1",
@@ -60,72 +67,71 @@ export class QuestionInfo extends RenderPairComponent<RouteComponentProps<any>, 
                 "d": "选项4",
             };
             return <div>
-                {this.renderPairInputText("question.sc.a", "选项1")}
-                {this.renderPairInputText("question.sc.b", "选项2")}
-                {this.renderPairInputText("question.sc.c", "选项3")}
-                {this.renderPairInputText("question.sc.d", "选项4")}
-                {this.renderPairSelect("question.answer", "正确答案", answerMap)}
+                {this.renderPairInputText("sc.a", "选项1")}
+                {this.renderPairInputText("sc.b", "选项2")}
+                {this.renderPairInputText("sc.c", "选项3")}
+                {this.renderPairInputText("sc.d", "选项4")}
+                {this.renderPairSelect("answer", "正确答案", answerMap)}
             </div>
-        } else if (this.state.question.ty == "tf") {
+        } else if (this.props.question.ty == "tf") {
             let answerMap = {
                 "": "请选择",
                 "true": "正确",
                 "false": "错误",
             };
             return <div>
-                {this.renderPairSelect("question.answer", "正确答案", answerMap)}
+                {this.renderPairSelect("answer", "正确答案", answerMap)}
             </div>
         }
     }
 
     renderType() {
         let map = questionTypeMap;
-        if (this.getQid() == "init") {
-            return this.renderPairSelect("question.ty", "题目类型", map)
+        if (!this.props.question.id) {
+            return this.renderPairSelect("ty", "题目类型", map)
         } else {
-            return <span>{map[this.state.question.ty]}</span>
+            return <span>{map[this.props.question.ty]}</span>
         }
     }
 
     render() {
-        if (!this.state.question) {
-            return <Modal/>
-        }
-        let submit = () => {
-            let qt = this.state.question;
-            console.log(qt);
-            if (qt.ty == "tf") {
-                delete qt.sc;
-            }
-            if (this.getQid() == "init") {
-                ajax({
-                    url: `/course/${this.getCid()}/question`,
-                    type: "POST",
-                    data: JSON.stringify(qt),
-                    success: () => {
-                        location.href = `/course/${this.getCid()}/question`
-                    }
-                })
-            } else {
-                ajax({
-                    url: `/course/${this.getCid()}/question/${this.getQid()}`,
-                    type: "PUT",
-                    data: JSON.stringify(qt),
-                    success: () => {
-                        location.href = `/course/${this.getCid()}/question`
-                    }
-                })
-            }
-        };
-        return <Modal>
-            <h1>新增题目</h1>
+        // if (!this.props.question) {
+        //     return <Modal/>
+        // }
+        // let submit = () => {
+        //     let qt = this.props.question;
+        //     console.log(qt);
+        //     if (qt.ty == "tf") {
+        //         delete qt.sc;
+        //     }
+        //     if (this.getQid() == "init") {
+        //         ajax({
+        //             url: `/course/${this.getCid()}/question`,
+        //             type: "POST",
+        //             data: JSON.stringify(qt),
+        //             success: () => {
+        //                 location.href = `/course/${this.getCid()}/question`
+        //             }
+        //         })
+        //     } else {
+        //         ajax({
+        //             url: `/course/${this.getCid()}/question/${this.getQid()}`,
+        //             type: "PUT",
+        //             data: JSON.stringify(qt),
+        //             success: () => {
+        //                 location.href = `/course/${this.getCid()}/question`
+        //             }
+        //         })
+        //     }
+        // };
+        return <div>
             {this.renderType()}
-            {this.renderPairTextArea("question.title", "题目")}
-            {this.renderPairInputText("question.score", "分值")}
+            {this.renderPairTextArea("title", "题目")}
+            {this.renderPairInputText("score", "分值")}
             {this.renderContent()}
-            <Link to={`/course/${this.getCid()}/question`}>取消</Link>
-            <button onClick={submit}>保存</button>
-        </Modal>
+            <button onClick={this.props.onSubmit}>保存</button>
+            <button onClick={this.props.onCancel}>取消</button>
+        </div>
     }
 }
 
--- src/Admin/Course/QuestionList/index.tsx
+++ src/Admin/Course/QuestionList/index.tsx
@@ -7,13 +7,68 @@ import {RenderPairComponent} from "../../../component/RenderPair/index";
 import * as _ from "lodash";
 import {RouteComponentProps} from "react-router";
 import {Link} from "react-router-dom";
-import {questionTypeMap} from "../../../def/data";
+import {JVOID0, questionTypeMap} from "../../../def/data";
+import {CurdComponent, CurdState} from "../../../common/curd-component";
+import {EH, TEH} from "../../../common/render-component";
+import {QuestionInfo} from "../QuestionInfo/index";
+import {update} from "../../../common/updater";
+
+
+class QuestionListInner extends CurdComponent<Question> {
+    constructor() {
+        super();
+        this.state = new CurdState<Question>();
+    }
+
+    idField(): string {
+        return "id";
+    }
+
+    urlSlice(): number {
+        return 4;
+    }
+
+    getHeaderRender(onCreate: EH, onUpdate: TEH<Question>, onDelete: TEH<Question>): Array<{ name: string; render: any }> {
+        return [{
+            name: "题目", render: "title",
+        }, {
+            name: "类型", render: (item) => questionTypeMap[item.ty],
+        }, {
+            name: "分值", render: "score",
+        }, {
+            name: "操作", render: (item: Question) => <div>
+                <a href={JVOID0} onClick={onUpdate.bind(null, item)}>查看</a>
+                <a href={JVOID0} onClick={onDelete.bind(null, item)}>删除</a>
+            </div>
+        }];
+    }
+
+    renderContent(renderTable: () => any, renderModal: () => any, onCreate: EH, onUpdate: TEH<Question>, onDelete: TEH<Question>): any {
+        return <div>
+            {renderTable()}
+            {renderModal()}
+            <button onClick={onCreate}>新增</button>
+        </div>
+    }
+
+    itemConstructor(): Question {
+        let q = new Question();
+        q.ty = "sc";
+        return q;
+    }
+
+    renderModalContent(onChange: TEH<Question>, onSubmit: EH, onCancel: EH): any {
+        return <QuestionInfo onSubmit={onSubmit} onCancel={onCancel} onChange={onChange} question={this.state.item}/>
+    }
+
+    getRenderRootMode(): { root: any; mode: string; onChange?: Function } {
+        return {root: this.state, mode: "state"};
+    }
+}
 
 class State {
     course: Course = new Course();
-    question: Question = null;
 }
-
 
 export class QuestionList extends RenderPairComponent<RouteComponentProps<any>, State> {
     getRenderRootMode(): { root: any; mode: string } {
@@ -39,60 +94,66 @@ export class QuestionList extends RenderPairComponent<RouteComponentProps<any>, 
         })
     }
 
-    renderModal() {
-        if (!this.state.question) {
-            return;
-        }
-        let submit = () => {
-            ajax({
-                url: `/course/${this.getCid()}/question`,
-                type: "POST",
-                data: JSON.stringify(this.state.question),
-                success: (res) => {
-                    let course = _.cloneDeep(this.state.course);
-                    course.questions.push(res);
-                    course.questionCount = course.questions.length;
-                    this.setState({course, question: null});
-                }
-            })
-        };
-        return <Modal>
-            {this.renderPairInputText("question.name", "名字")}
-            <button onClick={submit}>确定</button>
-        </Modal>
-    }
+    // renderModal() {
+    //     if (!this.state.question) {
+    //         return;
+    //     }
+    //     let submit = () => {
+    //         ajax({
+    //             url: `/course/${this.getCid()}/question`,
+    //             type: "POST",
+    //             data: JSON.stringify(this.state.question),
+    //             success: (res) => {
+    //                 let course = _.cloneDeep(this.state.course);
+    //                 course.questions.push(res);
+    //                 course.questionCount = course.questions.length;
+    //                 this.setState({course, question: null});
+    //             }
+    //         })
+    //     };
+    //     return <Modal>
+    //         {this.renderPairInputText("question.name", "名字")}
+    //         <button onClick={submit}>确定</button>
+    //     </Modal>
+    // }
 
     render() {
-        let onDelete = (item: Video) => {
-            ajax({
-                url: `/course/${this.getCid()}/question/${item.id}`,
-                type: "DELETE",
-                success: () => {
-                    // noinspection SillyAssignmentJS
-                    location.href = location.href;
-                }
-            })
+        let onChange = (list) => {
+            let state = update(this.state, "course.questions", list);
+            this.setState(state)
         };
-        let headers = [{
-            name: "题目", render: "title",
-        }, {
-            name: "类型", render: (item) => questionTypeMap[item.ty],
-        }, {
-            name: "分值", render: "score",
-        }, {
-            name: "操作", render: (item: Question) => <div>
-                <Link to={`/course/${this.getCid()}/question/${item.id}`}>查看</Link>
-                <a href="javascript:void(0)" onClick={onDelete.bind(this, item)}>删除</a>
-            </div>
-        }];
-        return <div>
-            <h1>题目</h1>
-            <Table list={this.state.course.questions}
-                   headers={headers}
-                   props={{className: "table"}}/>
-            <Link to={`/course/${this.getCid()}/question/init`} className="btn btn-primary">新增</Link>
-            {this.renderModal()}
-        </div>
+        return <QuestionListInner history={this.props.history} onChange={onChange}
+                                  list={this.state.course.questions}/>
+        // let onDelete = (item: Video) => {
+        //     ajax({
+        //         url: `/course/${this.getCid()}/question/${item.id}`,
+        //         type: "DELETE",
+        //         success: () => {
+        //             // noinspection SillyAssignmentJS
+        //             location.href = location.href;
+        //         }
+        //     })
+        // };
+        // let headers = [{
+        //     name: "题目", render: "title",
+        // }, {
+        //     name: "类型", render: (item) => questionTypeMap[item.ty],
+        // }, {
+        //     name: "分值", render: "score",
+        // }, {
+        //     name: "操作", render: (item: Question) => <div>
+        //         <Link to={`/course/${this.getCid()}/question/${item.id}`}>查看</Link>
+        //         <a href="javascript:void(0)" onClick={onDelete.bind(this, item)}>删除</a>
+        //     </div>
+        // }];
+        // return <div>
+        //     <h1>题目</h1>
+        //     <Table list={this.state.course.questions}
+        //            headers={headers}
+        //            props={{className: "table"}}/>
+        //     <Link to={`/course/${this.getCid()}/question/init`} className="btn btn-primary">新增</Link>
+        //     {this.renderModal()}
+        // </div>
     }
 }
 
