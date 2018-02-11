863f310395f2153c6e95fc0f7783f32b90482a29
yuanxiaoyan <yuanxiaoyan@qq.com>
2018-02-11 10:29:05 +0800

    Modify: admin-登录页面-添加样式

--- src/Admin/AdminLogin/index.tsx
+++ src/Admin/AdminLogin/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {RenderPairComponent} from "../../component/RenderPair/index";
 import {ajaxPost} from "../../common/kit";
 import {RouteComponentProps} from "react-router";
+import './style.less';
 
 class State {
     user = {};
@@ -23,11 +24,19 @@ export class AdminLogin extends RenderPairComponent<RouteComponentProps<any>, St
                 this.props.history.push(`/admin/course-category`)
             })
         };
-        return <div>
-            <h1>Login Page</h1>
-            {this.renderPairInputText("user.username", "用户名")}
-            {this.renderPairInputPassword("user.password", "密码")}
-            <button onClick={login}>登录</button>
+        return <div className={'login-box admin-login'}>
+            <div className={'login-logo'}><h1>管理员登录</h1></div>
+            <div className={'login-box-body'}>
+                <div className={'form-group has-feedback'}>
+                    {this.renderInputText("user.username", "用户名")}
+                    <span className={'glyphicon glyphicon-envelope form-control-feedback'}/>
+                </div>
+                <div className={'form-group has-feedback'}>
+                    {this.renderInputPassword("user.password", "密码")}
+                    <span className={'glyphicon glyphicon-lock form-control-feedback'}/>
+                </div>
+                <button onClick={login} className={'btn btn-primary btn-block'}>登录</button>
+            </div>
         </div>
     }
 }
